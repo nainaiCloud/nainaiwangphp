2c02979357baf2ef03020bfc59d1a16a6eac2f70
weipinglee <weiping.lee@163.com>
2018-08-15 12:24:33 +0800

    jsmock delete

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -36,24 +36,24 @@ class TestController extends \Yaf\Controller_Abstract{
 
 
     public function jsMockAction(){
-        if(IS_POST){
-            $username = safe::filterPost('username');
-            $amount = safe::filterPost('amount');
-            $obj = new \nainai\fund\jsMock();
-            $userObj = new \Library\M('user');
-            $user_id = $userObj->where(array('username'=>$username))->getField('id');
-            if(!$user_id){
-                die(json_encode(tool::getSuccInfo(0,'用户不存在')));
-            }
-            $res = $obj->payMarket($user_id,$amount);
-            if($res){
-                die(json_encode(tool::getSuccInfo()));
-            }else{
-                die(json_encode(tool::getSuccInfo(0,'失败')));
-            }
-        }else{
-
-        }
+//        if(IS_POST){
+//            $username = safe::filterPost('username');
+//            $amount = safe::filterPost('amount');
+//            $obj = new \nainai\fund\jsMock();
+//            $userObj = new \Library\M('user');
+//            $user_id = $userObj->where(array('username'=>$username))->getField('id');
+//            if(!$user_id){
+//                die(json_encode(tool::getSuccInfo(0,'用户不存在')));
+//            }
+//            $res = $obj->payMarket($user_id,$amount);
+//            if($res){
+//                die(json_encode(tool::getSuccInfo()));
+//            }else{
+//                die(json_encode(tool::getSuccInfo(0,'失败')));
+//            }
+//        }else{
+//
+//        }
 
     }
 
